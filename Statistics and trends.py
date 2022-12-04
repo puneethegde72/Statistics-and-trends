34955bd1a88a1c070ef45756dc45e075c8f1f193
hegdepuneet72 <116311971+hegdepuneet72@users.noreply.github.com>
2022-12-04 03:41:35 +0000

    Update Statistics and trends.py

--- Statistics and trends.py
+++ Statistics and trends.py
@@ -14,7 +14,7 @@ def dataFrame(file_name):
    # df= df.drop(df.iloc[:, -6:],axis=1)
     a = df['Country Name']
     #print(a)
-    df= df.iloc[5:15, 45:55]
+    df= df.iloc[8:18, 45:55]
     df.insert(loc=0, column='Country Name', value=a)
     #df['Country Name']= a
     df= df.dropna(axis = 0)
@@ -26,6 +26,13 @@ def dataFrame(file_name):
 a,b = dataFrame("API_EN.ATM.CO2E.LF.KT_DS2_en_csv_v2_4538194.csv")
 print(a)
 print(b)
-c,d = a,b = dataFrame("API_EG.ELC.ACCS.ZS_DS2_en_csv_v2_4695288.csv")
+c,d = dataFrame("API_EG.ELC.ACCS.ZS_DS2_en_csv_v2_4695288.csv")
 print(c)
 print(d)
+def Bar_p(value_x, value_y):
+    a.plot.bar(x = value_x,
+                    y= value_y,
+                    colormap='viridis', legend=True, figsize=(15,8), rot=45 )
+    return
+a["Average"] = a.mean(axis = 1,numeric_only= True)
+Bar_p("Country Name", "Average")
