4086d5d74f7281ed6a29134980966f3ad5f4a8d2
Puneethegde72 <punihegde72@gmail.com>
2022-12-11 03:27:34 +0000

    Update Statistics and trends.py

--- Statistics and trends.py
+++ Statistics and trends.py
@@ -49,44 +49,62 @@ def stats_f(file_name,years,col, value1):
     df1= df1.dropna(axis = 0)
     #Droping the NAN values from daraframe Row wise
     df1= df1.dropna(axis = 1)
+    #transposing the index of the dataframe
     df2 = df1.set_index("Indicator Name").T
-    #df2.columns=df2.iloc[0]
+    #returning dataframe requred for Heatmap
     return df2
-
+# years using for the data analysis
 years = [35,40,45,50,55,60,64]
+# countries which are using for data analysis
 countries=[35,40,55,81,109,119,202,205,251]
-population_c,population_y =dataFrame("API_19_DS2_en_csv_v2_4700503.csv",years,countries,"Indicator Name","Population, total")
-Green_gas_c,Green_gas_y = dataFrame("API_19_DS2_en_csv_v2_4700503.csv",years,countries,"Indicator Name","Total greenhouse gas emissions (kt of CO2 equivalent)")
-co2_c,co2_y = dataFrame("API_19_DS2_en_csv_v2_4700503.csv",years,countries,"Indicator Name","CO2 emissions (kt)")
-Up_c,Up_y = dataFrame("API_19_DS2_en_csv_v2_4700503.csv",years,countries,"Indicator Name","Urban population")
-india = stats_f("API_19_DS2_en_csv_v2_4700503.csv",[3,35,40,45,50,55,60,64],"Country Name","India")
-print("india/n", india)
-
-
-
+'''calling dataFrame functions for all the dataframe which will be used for visualization'''
+population_c,population_y =dataFrame("API_19_DS2_en_csv_v2_4700503.csv",years,
+                                     countries,"Indicator Name","Population, total")
+Green_gas_c,Green_gas_y = dataFrame("API_19_DS2_en_csv_v2_4700503.csv",years,
+                                    countries,"Indicator Name","Total greenhouse gas emissions (kt of CO2 equivalent)")
+co2_c,co2_y = dataFrame("API_19_DS2_en_csv_v2_4700503.csv",years,
+                        countries,"Indicator Name","CO2 emissions (kt)")
+Up_c,Up_y = dataFrame("API_19_DS2_en_csv_v2_4700503.csv",years,
+                      countries,"Indicator Name","Urban population")
+india = stats_f("API_19_DS2_en_csv_v2_4700503.csv",[3,35,40,45,50,55,60,64],
+                "Country Name","India")
+'''with this function Ploting a bar graph using pandas ploting meathod'''
 def plot_p(DataFrame, col, types, name):
-    ax=DataFrame.plot(x=col, rot=45, figsize=(50,25), kind= types, title= name,fontsize=30)
+    #ploting graph with the parameater which given in the function
+    ax=DataFrame.plot(x=col, rot=45, figsize=(50,25),
+                      kind= types, title= name,fontsize=30)
+    #setting legend font size
     ax.legend(fontsize=36)
+    #setting Title and font size for a plot
     ax.set_title(name,pad=20, fontdict={'fontsize':40})
     return
-
+# ploting the bar graph1
 plot_p(population_c,"Country Name","bar","Total population")
+# Saving plot
 plt.savefig('Total population.jpg')
 plot_p(Green_gas_c,"Country Name", "bar","Total greenhouse gas emissions (kt of CO2 equivalent)")
 plt.savefig('Total greenhouse gas emissions.jpg')
 
 legend_properties = {'weight':'bold','size':36}
+# ploting the bar graph-2
 ax1 = Up_y.plot(figsize=(60,30),kind="line",fontsize=36,linewidth=4.0)
+#setting a title for the graph
 ax1.set_title("Total Urban population",pad=20, fontdict={'fontsize':40})
 ax1.legend(loc=2,prop=legend_properties)
+# Saving plot
 plt.savefig('Total Urban population line.jpg')
 
-
+#creating a line graph using pandas
 ax2 = co2_y.plot(figsize=(60,30),kind="line",fontsize=36,linewidth=4.0)
+#setting a Title for a graph
 ax2.set_title("Total Co2 Emission",pad=30, fontdict={'fontsize':40})
+#setting legends for a graph
 ax2.legend(prop=legend_properties)
+# Saving plot
 plt.savefig('Total Co2 Emission line.jpg')
+# creating a variable x for Heat Map
 x=["Population, total","Urban population","Foreign direct investment, net inflows (% of GDP)","CO2 emissions (kt)"]
+# filtering the columns and rows as per the heatmap requirement
 india = india.loc[:,x ]
 
 print(india.corr())
